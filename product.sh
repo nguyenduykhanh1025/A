ba3122bfb54b4dadfbf0b7adbb966ab1b349c803
Nguyen Duy Khanh <u.khanhduy.nguyen@ext.ewerk.com>
2023-08-07 15:51:42 +0700

    push

--- product.sh
+++ product.sh
@@ -1,7 +1,13 @@
 PUBLISHABLES_LIBS='product'
 LIB_NAME=$(npx nx print-affected --type=lib --select=projects --plain)
 if [[ " ${PUBLISHABLES_LIBS} " == *" $LIB_NAME "* ]]; then
-  git branch -D "feature/auto-update-dependencies"
+  echo "Delete old dependencies update branch..."
+  git push origin --delete "feature/auto-update-dependencies" || true
+  git branch -D "feature/auto-update-dependencies" || true
+
+  echo "Create fresh update branch..."
+  git checkout -q -b "feature/auto-update-dependencies"
+
   npx nx release product
   exit 1
 else
