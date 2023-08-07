4147638118550c518e882122060dc274f546ba20
Nguyen Duy Khanh <u.khanhduy.nguyen@ext.ewerk.com>
2023-08-07 15:49:50 +0700

    push

--- product.sh
+++ product.sh
@@ -1,6 +1,7 @@
 PUBLISHABLES_LIBS='product'
 LIB_NAME=$(npx nx print-affected --type=lib --select=projects --plain)
 if [[ " ${PUBLISHABLES_LIBS} " == *" $LIB_NAME "* ]]; then
+  git branch -D "feature/auto-update-dependencies"
   npx nx release product
   exit 1
 else
