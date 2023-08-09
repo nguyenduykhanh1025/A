a3d550e80335de5dcb0770bfcb06ae24cc82c4d0
Nguyen Duy Khanh <u.khanhduy.nguyen@ext.ewerk.com>
2023-08-09 09:40:08 +0700

    push

--- product.sh
+++ product.sh
@@ -3,6 +3,8 @@ LIB_NAME=$(npx nx print-affected --type=lib --select=projects --plain)
 for PUBLISHABLES_LIB in ${PUBLISHABLES_LIBS[@]}
 do
   if [[ "$LIB_NAME" == *"$PUBLISHABLES_LIB"* ]]; then
+    git checkout main
+
     echo "Delete old dependencies update branch..."
     git push origin --delete "feature/auto-update-$PUBLISHABLES_LIB-version" || true
     git branch -D "feature/auto-update-$PUBLISHABLES_LIB-version" || true
