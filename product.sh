7e6b9fda7933a98224083ba5c3c039ecb317e31f
Nguyen Duy Khanh <u.khanhduy.nguyen@ext.ewerk.com>
2023-08-15 10:15:50 +0700

    chore(repo): release product 0.0.1-somethingname.0 aloa

--- product.sh
+++ product.sh
@@ -1,6 +1,8 @@
 MASTER_BASE='main'
 LIB_NAME='product'
 AFFECTED_LIBS=$(npx nx print-affected --type=lib --select=projects --base=$MASTER_BASE --head=HEAD --plain)
+GIT_BRANCH_CURRENT_REALLY=$(git rev-parse --abbrev-ref HEAD)
+timestamp=$(date +%s)
 GIT_BRANCH_CURRENT='main'
 git checkout $GIT_BRANCH_CURRENT
 
@@ -18,7 +20,7 @@ if [[ "$AFFECTED_LIBS" == *"$LIB_NAME"* ]]; then
   git checkout -q -b "feature/auto-update-$LIB_NAME-version"
 
   git push --set-upstream origin "feature/auto-update-$LIB_NAME-version"
-  npx nx release $LIB_NAME
+  npx nx release $LIB_NAME --preRelease="$GIT_BRANCH_CURRENT_REALLY.$timestamp"
 
   echo "Start to publish $LIB_NAME library to npm"
   ./scripts/build-and-publish-lib.sh $LIB_NAME --dry-run
