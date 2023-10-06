1fea66f1c404422993b01aafa8afd09dfd5cfc73
Nguyen Duy Khanh <u.khanhduy.nguyen@ext.ewerk.com>
2023-10-06 15:13:37 +0700

    chore(repo): release product 0.3.2-auto-update-product-version.1692073184.0

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 # ARGS
 LIB_NAME=$1
 ADDITIONAL_ARGS=${@:2}
-GIT_BRANCH_CURRENT_NAME=$(git rev-parse --abbrev-ref HEAD | cut -d'/' -f 2)
+GIT_BRANCH_CURRENT_NAME='feature/DUBASDK-344-sdk-frontend-create-display-tabular-data2'
 timestamp=$(date +%s)
 
 # CONST
@@ -9,8 +9,9 @@ PUBLISHABLES_LIBS=("product" "common-ui")
 MASTER_BASE=main
 
 AFFECTED_LIBS=$(npx nx print-affected --type=lib --select=projects --base=$MASTER_BASE --head=HEAD --plain)
-echo $LIB_NAME
-if [[ "$AFFECTED_LIBS" == *"$LIB_NAME"* ]]; then
+# echo $AFFECTED_LIBS
+# echo $LIB_NAME
+# if [[ "$AFFECTED_LIBS" == *"$LIB_NAME"* ]]; then
   echo "Checkout to $GIT_BRANCH_CURRENT before creating a new branch for changing the version of $LIB_NAME"
   git checkout $GIT_BRANCH_CURRENT
 
@@ -25,5 +26,5 @@ if [[ "$AFFECTED_LIBS" == *"$LIB_NAME"* ]]; then
   echo '----------------'
   echo $GIT_BRANCH_CURRENT_NAME
   npx nx release $LIB_NAME prepatch --preReleaseId="$GIT_BRANCH_CURRENT_NAME.$timestamp"
-fi
+# fi
 echo -e "✅ Done. ✅️"
